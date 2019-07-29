a7b795e80347b40a41012944605280d561f8def9
briankiplimo <limobrian290@gmail.com>
2019-07-29 12:27:15 +0300

    function that takes result and transforms to list ,

--- app/request.py
+++ app/request.py
@@ -12,16 +12,16 @@ def get_sources():
     '''
     Function that get the json request
     '''
-#     get_sources_url = base_url.format(api_key)
-#     with urllib.request.urlopen(get_sources_url) as url:
-#         get_sources_data = url.read()
-#         get_sources_response = json.loads(get_sources_data)
-#         sources_results = None
-#         if get_sources_response['sources']:
-#             sources_results_list = get_sources_response['sources']
-#             sources_results = process_results(sources_results_list)
-#     return sources_results
-# def process_results(source_list):
+    get_sources_url = base_url.format(api_key)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+        sources_results = None
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+    return sources_results
+def process_results(source_list):
     '''
     Function that get sources result and transform them to list
     Args:
@@ -29,15 +29,15 @@ def get_sources():
     Returns:
     source_results:A list of sources objects
     '''
-    source_results = []
-    for source_item in source_list:
-        id = source_item.get('id')
-        name = source_item.get('name')
-        description = source_item.get('description')
-        source_object = Source(id,name,description)
-        source_results.append(source_object)
-    return source_results
-def get_articles(id):
+#     source_results = []
+#     for source_item in source_list:
+#         id = source_item.get('id')
+#         name = source_item.get('name')
+#         description = source_item.get('description')
+#         source_object = Source(id,name,description)
+#         source_results.append(source_object)
+#     return source_results
+# def get_articles(id):
     '''
     Function that gets json response to url request
     '''
