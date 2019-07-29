5699e241555e76c06a12965e93a2fdfb43e6a893
briankiplimo <limobrian290@gmail.com>
2019-07-29 12:38:12 +0300

    method that runs be4 every test

--- newssource_test.py
+++ newssource_test.py
@@ -4,12 +4,12 @@ class SourceTest(unittest.TestCase):
     '''
     Test of the class to test its behaviour
     '''
-    # def setUp(self):
+    def setUp(self):
         '''
         Setup method that runs before every test
         '''
-        self.new_source = Source('ABC News','Best leading source','www.abcnews.com')
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_source,Source))
-if __name__== '__main__':
-    unittest.main()
+#         self.new_source = Source('ABC News','Best leading source','www.abcnews.com')
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_source,Source))
+# if __name__== '__main__':
+#     unittest.main()
