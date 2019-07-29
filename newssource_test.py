074d9c8be1ed35c6ebf1beee0e82d36ce5b14de3
briankiplimo <limobrian290@gmail.com>
2019-07-29 12:55:38 +0300

    Deployment

--- newssource_test.py
+++ newssource_test.py
@@ -8,8 +8,8 @@ class SourceTest(unittest.TestCase):
         '''
         Setup method that runs before every test
         '''
-#         self.new_source = Source('ABC News','Best leading source','www.abcnews.com')
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_source,Source))
-# if __name__== '__main__':
-#     unittest.main()
+        self.new_source = Source('ABC News','Best leading source','www.abcnews.com')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+if __name__== '__main__':
+    unittest.main()
