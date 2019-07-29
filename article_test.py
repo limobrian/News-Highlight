8dd0f786739a183b84471b7205d0297f2ce24e18
briankiplimo <limobrian290@gmail.com>
2019-07-29 12:30:08 +0300

    config parent

--- article_test.py
+++ article_test.py
@@ -8,10 +8,10 @@ class ArticleTest(unittest.TestCase):
         '''
         Set up method that will run before test
         '''
-#         self.new_article = Article('Washington post','null','Haiti earthquake','null','2018.10.07','null','null')
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_article,Article))
+        self.new_article = Article('Washington post','null','Haiti earthquake','null','2018.10.07','null','null')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
         
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
     
