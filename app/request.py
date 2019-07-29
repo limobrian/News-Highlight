21b5c47faf1f25f0c26c74dc4834a07d1d2132d0
briankiplimo <limobrian290@gmail.com>
2019-07-29 12:26:09 +0300

    Function that get the json request

--- app/request.py
+++ app/request.py
@@ -12,16 +12,16 @@ def get_sources():
     '''
     Function that get the json request
     '''
-    get_sources_url = base_url.format(api_key)
-    with urllib.request.urlopen(get_sources_url) as url:
-        get_sources_data = url.read()
-        get_sources_response = json.loads(get_sources_data)
-        sources_results = None
-        if get_sources_response['sources']:
-            sources_results_list = get_sources_response['sources']
-            sources_results = process_results(sources_results_list)
-    return sources_results
-def process_results(source_list):
+#     get_sources_url = base_url.format(api_key)
+#     with urllib.request.urlopen(get_sources_url) as url:
+#         get_sources_data = url.read()
+#         get_sources_response = json.loads(get_sources_data)
+#         sources_results = None
+#         if get_sources_response['sources']:
+#             sources_results_list = get_sources_response['sources']
+#             sources_results = process_results(sources_results_list)
+#     return sources_results
+# def process_results(source_list):
     '''
     Function that get sources result and transform them to list
     Args:
