c9d5068d8b45baf79122419a1a0ba06943e441bb
briankiplimo <limobrian290@gmail.com>
2019-07-29 12:36:08 +0300

    config:The parent configuration class

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ class Config:
     '''
     Main configuration parent class
     '''
-    # NEWSHIGHLIGHT_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
-    # NEWSHIGHLIGHT_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    # NEWSHIGHLIGHT_API_KEY = os.environ.get('NEWSHIGHLIGHT_API_KEY')
+    NEWSHIGHLIGHT_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWSHIGHLIGHT_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWSHIGHLIGHT_API_KEY = os.environ.get('NEWSHIGHLIGHT_API_KEY')
    
 class ProdConfig(Config):
     '''
--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ manager.add_command('server',Server())
 @manager.command
 def test():
     """Run test"""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verboity=2).run(tests)
-if __name__ == '__main__':
-    manager.run()
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verboity=2).run(tests)
+# if __name__ == '__main__':
+#     manager.run()
